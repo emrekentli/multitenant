3cfc24d98ffabda1becac7c6edf43b597856f9a6
Emre Kentli <emrekentlii@gmail.com>
2025-01-14 14:47:20 +0300

    initial commit

--- app/middlewares/middlewares.go
+++ app/middlewares/middlewares.go
@@ -6,17 +6,25 @@ import (
 	"github.com/gofiber/fiber/v3/middleware/compress"
 	"github.com/gofiber/fiber/v3/middleware/cors"
 	"github.com/gofiber/fiber/v3/middleware/etag"
+	"github.com/gofiber/fiber/v3/middleware/helmet"
+	"github.com/gofiber/fiber/v3/middleware/logger"
 	"github.com/gofiber/fiber/v3/middleware/static"
+	"os"
 )
 
 func RegisterBuiltInMiddlewares(s *fiber.App) {
 	s.Use(etag.New())
-
+	s.Use(helmet.New())
+	s.Use(compress.New(compress.Config{
+		Level: compress.LevelBestSpeed, // 1
+	}))
+	if os.Getenv("ENABLE_LOGGER") == "true" {
+		s.Use(logger.New())
+	}
 	s.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:5173"},
 		AllowCredentials: true,
 	}))
-	s.Use(compress.New())
 	jwt.RegisterJwtMiddleware(s)
 	s.Get("/*", static.New("./public"))
 }
